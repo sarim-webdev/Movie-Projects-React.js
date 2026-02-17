c7934d57fe99acf358b60db99916b6effa8a34de
Muhammad Sarim <muhammadsarim1861@gmail.com>
2026-02-17 14:58:45 +0500

    react

---
+++ src/Data/MovieCards.js
@@ -0,0 +1,16 @@
+// MovieCards.js
+
+const movies = [
+  { id: 1, image: "/images/movie1.jpg" },
+  { id: 2, image: "/images/movie2.jpg" },
+  { id: 3, image: "/images/movie3.jpg" },
+  { id: 4, image: "/images/movie4.jpg" },
+  { id: 5, image: "/images/movie5.jpg" },
+  { id: 6, image: "/images/movie6.jpg" },
+  { id: 7, image: "/images/movie7.jpg" },
+  { id: 8, image: "/images/movie8.jpg" },
+  { id: 9, image: "/images/movie9.jpg" },
+  { id: 10, image: "/images/movie10.jpg" },
+];
+
+export default movies;
