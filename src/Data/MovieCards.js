938741e1055b7a9aa9f0b00fa680bbb59e97908c
Muhammad Sarim <muhammadsarim1861@gmail.com>
2026-02-23 23:24:09 +0500

    react

--- src/Data/MovieCards.js
+++ src/Data/MovieCards.js
@@ -1,55 +1,138 @@
-import TrendingImageone from "../assets/movie-1.jpg"
-import TrendingImagetwo from "../assets/movie-2.jpg"
-import TrendingImagethree from "../assets/movie-3.jpg"
-import TrendingImagefour from "../assets/movie-4.jpg"
-import TrendingImagefive from "../assets/movie-5.jpg"
-import TrendingImagesix from "../assets/movie-6.jpg"
-import TrendingImageseven from "../assets/movie-7.jpg"
-import TrendingImageeight from "../assets/movie-8.jpg"
-import TrendingImagenine from "../assets/movie-9.jpg"
-import TrendingImageten from "../assets/movie-10.jpg"
+import movie1 from "../assets/movie-1.jpg"
+import movie2 from "../assets/movie-2.jpg"
+import movie3 from "../assets/movie-3.jpg"
+import movie4 from "../assets/movie-4.jpg"
+import movie5 from "../assets/movie-5.jpg"
+import movie6 from "../assets/movie-6.jpg"
+import movie7 from "../assets/movie-7.jpg"
+import movie8 from "../assets/movie-8.jpg"
+import movie9 from "../assets/movie-9.jpg"
+import movie10 from "../assets/movie-10.jpg"
 
-const trendingCard = [
-  {
-    id: 1,
-    image: TrendingImageone
-  },
-  {
-    id: 2,
-    image: TrendingImagetwo
-  },
-  {
-    id: 3,
-    image: TrendingImagethree
-  },
-  {
-    id: 4,
-    image: TrendingImagefour
-  },
-  {
-    id: 5,
-    image: TrendingImagefive
-  },
-  {
-    id: 6,
-    image: TrendingImagesix
-  },
-  {
-    id: 7,
-    image: TrendingImageseven
-  },
-  {
-    id: 8,
-    image: TrendingImageeight
-  },
-  {
-    id: 9,
-    image: TrendingImagenine
-  },
-  {
-    id: 10,
-    image: TrendingImageten
-  },
-];
+/* Trending Movies */
 
-export default trendingCard;
+const trendingMovies = [
+  { id: 1, image: movie1 },
+  { id: 2, image: movie2 },
+  { id: 3, image: movie3 },
+  { id: 4, image: movie4 },
+  { id: 5, image: movie5 },
+  { id: 6, image: movie6 },
+  { id: 7, image: movie7 },
+  { id: 8, image: movie8 },
+  { id: 9, image: movie9 },
+  { id: 10, image: movie10 },
+]
+
+/* Latest Movies */
+
+const latestMovies = [
+  { id: 1, image: movie1 },
+  { id: 2, image: movie2 },
+  { id: 3, image: movie3 },
+  { id: 4, image: movie4 },
+  { id: 5, image: movie5 },
+  { id: 6, image: movie6 },
+  { id: 7, image: movie7 },
+  { id: 8, image: movie8 },
+  { id: 9, image: movie9 },
+  { id: 10, image: movie10 },
+  { id: 11, image: movie1 },
+  { id: 12, image: movie2 },
+  { id: 13, image: movie3 },
+  { id: 14, image: movie4 },
+  { id: 15, image: movie5 },
+  { id: 16, image: movie6 },
+  { id: 17, image: movie7 },
+  { id: 18, image: movie8 },
+  { id: 19, image: movie9 },
+  { id: 20, image: movie10 },
+]
+
+/* Featured */
+
+const featuredMovies = [
+  { id: 1, image: movie2 },
+  { id: 2, image: movie4 },
+  { id: 3, image: movie6 },
+  { id: 4, image: movie8 },
+  { id: 5, image: movie10 },
+  { id: 6, image: movie1 },
+  { id: 7, image: movie3 },
+  { id: 8, image: movie5 },
+  { id: 9, image: movie7 },
+  { id: 10, image: movie9 },
+  { id: 11, image: movie2 },
+  { id: 12, image: movie4 },
+  { id: 13, image: movie6 },
+  { id: 14, image: movie8 },
+  { id: 15, image: movie10 },
+]
+
+/* Crime */
+
+const crimeMovies = latestMovies
+
+/* Action */
+
+const actionMovies = [
+  { id: 1, image: movie10 },
+  { id: 2, image: movie9 },
+  { id: 3, image: movie8 },
+  { id: 4, image: movie7 },
+  { id: 5, image: movie6 },
+  { id: 6, image: movie5 },
+  { id: 7, image: movie4 },
+  { id: 8, image: movie3 },
+  { id: 9, image: movie2 },
+  { id: 10, image: movie1 },
+]
+
+/* Romance */
+
+const romanceMovies = featuredMovies
+
+/* Comedy */
+
+const comedyMovies = [
+  { id: 1, image: movie8 },
+  { id: 2, image: movie6 },
+  { id: 3, image: movie4 },
+  { id: 4, image: movie2 },
+  { id: 5, image: movie10 },
+  { id: 6, image: movie1 },
+  { id: 7, image: movie3 },
+  { id: 8, image: movie5 },
+  { id: 9, image: movie7 },
+  { id: 10, image: movie9 },
+  { id: 11, image: movie8 },
+  { id: 12, image: movie6 },
+  { id: 13, image: movie4 },
+  { id: 14, image: movie2 },
+  { id: 15, image: movie10 },
+  { id: 16, image: movie1 },
+  { id: 17, image: movie3 },
+  { id: 18, image: movie5 },
+]
+
+export {
+  trendingMovies,
+  latestMovies,
+  featuredMovies,
+  crimeMovies,
+  actionMovies,
+  romanceMovies,
+  comedyMovies
+}
+
+/* default export */
+
+export default {
+  trendingMovies,
+  latestMovies,
+  featuredMovies,
+  crimeMovies,
+  actionMovies,
+  romanceMovies,
+  comedyMovies
+}
--- src/Pages/Movies.jsx
+++ src/Pages/Movies.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useRef } from "react";
+
+import {
+  latestMovies,
+  featuredMovies,
+  crimeMovies,
+  actionMovies,
+  romanceMovies,
+  comedyMovies,
+} from "../Data/MovieCards";
+
+const MovieRow = ({ title, data }) => {
+  const rowRef = useRef();
+
+  const scrollLeft = () => {
+    rowRef.current.scrollBy({
+      left: -800,
+      behavior: "smooth",
+    });
+  };
+
+  const scrollRight = () => {
+    rowRef.current.scrollBy({
+      left: 800,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div className="movieSection">
+
+      <h2 className="movieTitle">{title}</h2>
+
+      <div className="sliderWrapper">
+
+        <button className="arrow left" onClick={scrollLeft}>
+          ◀
+        </button>
+
+        <div className="movieRow" ref={rowRef}>
+          {data.map((movie) => (
+            <div className="movieCard" key={movie.id}>
+              <img src={movie.image} alt="" />
+            </div>
+          ))}
+        </div>
+
+        <button className="arrow right" onClick={scrollRight}>
+          ▶
+        </button>
+
+      </div>
+    </div>
+  );
+};
 
 const Movies = () => {
   return (
-    <div>Movies</div>
-  )
-}
+    <div className="moviesPage">
 
-export default Movies
+      <MovieRow title="Latest Movies" data={latestMovies} />
+
+      <MovieRow
+        title="Featured Originals and Exclusives"
+        data={featuredMovies}
+      />
+
+      <MovieRow title="Crime Movies" data={crimeMovies} />
+
+      <MovieRow
+        title="Action and Adventure Movies"
+        data={actionMovies}
+      />
+
+      <MovieRow title="Romance Movies" data={romanceMovies} />
+
+      <MovieRow title="Comedy Movies" data={comedyMovies} />
+
+    </div>
+  );
+};
+
+export default Movies;
