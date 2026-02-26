531a3a82e4bc4ef93f64af83fd77d79188d91e99
Muhammad Sarim <muhammadsarim1861@gmail.com>
2026-02-26 22:31:23 +0500

    react

--- src/Data/MovieCards.js
+++ src/Data/MovieCards.js
@@ -1,3 +1,5 @@
+/* =============== Movies page Data ================= */
+
 /* Trending Movies import */
 import trendingmovie1 from "../assets/movie-1.jpg"
 import trendingmovie2 from "../assets/movie-2.jpg"
@@ -98,8 +100,6 @@ const trendingCard = [
   { id: 10, image: trendingmovie10 },
 ]
 
-/* Latest Movies */
-
 const latestMovies = [
   { id: 1, image: latestmovie1 },
   { id: 2, image: latestmovie2 },
@@ -123,8 +123,6 @@ const latestMovies = [
   { id: 20, image: latestmovie20 },
 ]
 
-/* Featured */
-
 const featuredMovies = [
   { id: 1, image: featuredmovie1 },
   { id: 2, image: featuredmovie2 },
@@ -143,11 +141,9 @@ const featuredMovies = [
   { id: 15, image: featuredmovie15 },
 ]
 
-/* Crime */
 
 const crimeMovies = latestMovies
 
-/* Action */
 
 const actionMovies = [
   { id: 1, image: actionmovie1 },
@@ -162,11 +158,7 @@ const actionMovies = [
   { id: 10, image: actionmovie10 },
 ]
 
-/* Romance */
-
 const romanceMovies = featuredMovies
-
-/* Comedy */
 
 const comedyMovies = [
   { id: 1, image: comedymovie1 },
@@ -199,3 +191,63 @@ export {
   romanceMovies,
   comedyMovies
 }
+
+/* =============== TV show page Data ============== */
+
+import movie1 from "../assets/movie-1.jpg"
+import movie2 from "../assets/movie-2.jpg"
+import movie3 from "../assets/movie-3.jpg"
+import movie4 from "../assets/movie-4.jpg"
+import movie5 from "../assets/movie-5.jpg"
+import movie6 from "../assets/movie-6.jpg"
+import movie7 from "../assets/movie-7.jpg"
+import movie8 from "../assets/movie-8.jpg"
+import movie9 from "../assets/movie-9.jpg"
+import movie10 from "../assets/movie-10.jpg"
+
+/* Latest TV - 10 */
+export const latestTV = [
+  { id: 1, image: movie1 },
+  { id: 2, image: movie2 },
+  { id: 3, image: movie3 },
+  { id: 4, image: movie4 },
+  { id: 5, image: movie5 },
+  { id: 6, image: movie6 },
+  { id: 7, image: movie7 },
+  { id: 8, image: movie8 },
+  { id: 9, image: movie9 },
+  { id: 10, image: movie10 },
+]
+
+/* Anime TV - 15 */
+export const animeTV = [
+  ...latestTV,
+  { id: 11, image: movie1 },
+  { id: 12, image: movie2 },
+  { id: 13, image: movie3 },
+  { id: 14, image: movie4 },
+  { id: 15, image: movie5 },
+]
+
+/* Crime TV - 12 */
+export const crimeTV = [
+  ...latestTV.slice(0, 10),
+  { id: 11, image: movie6 },
+  { id: 12, image: movie7 },
+]
+
+/* Drama TV - 10 */
+export const dramaTV = [...latestTV]
+
+/* Romance TV - 8 */
+export const romanceTV = latestTV.slice(0, 8)
+
+/* Mystery & Thriller TV - 15 */
+export const mysteryTV = [
+  ...latestTV,
+  { id: 11, image: movie8 },
+  { id: 12, image: movie9 },
+  { id: 13, image: movie10 },
+  { id: 14, image: movie1 },
+  { id: 15, image: movie2 },
+]
--- src/Pages/Tvshow.jsx
+++ src/Pages/Tvshow.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useRef } from "react";
+
+import {
+  latestTV,
+  animeTV,
+  crimeTV,
+  dramaTV,
+  romanceTV,
+  mysteryTV
+} from "../Data/MovieCards";
+
+const TVRow = ({ title, data }) => {
+  const rowRef = useRef();
+
+  const scrollLeft = () => {
+    rowRef.current.scrollBy({
+      left: -800,
+      behavior: "smooth",
+    });
+  };
+
+  const scrollRight = () => {
+    rowRef.current.scrollBy({
+      left: 800,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div className="movieSection">
+
+      <h2 className="movieTitle">{title}</h2>
+
+      <div className="sliderWrapper">
+
+        <button className="arrow left" onClick={scrollLeft}>
+          ◀
+        </button>
+
+        <div className="movieRow" ref={rowRef}>
+          {data.map((item) => (
+            <div className="movieCard" key={item.id}>
+              <img src={item.image} alt="" />
+            </div>
+          ))}
+        </div>
+
+        <button className="arrow right" onClick={scrollRight}>
+          ▶
+        </button>
+
+      </div>
+    </div>
+  );
+};
 
 const Tvshow = () => {
   return (
-    <div>Tvshow</div>
-  )
-}
+    <div className="moviesPage">
 
-export default Tvshow
+      <TVRow title="Latest TV" data={latestTV} />
+      <TVRow title="Anime TV" data={animeTV} />
+      <TVRow title="Crime TV" data={crimeTV} />
+      <TVRow title="Drama TV" data={dramaTV} />
+      <TVRow title="Romance TV" data={romanceTV} />
+      <TVRow title="Mystery and Thriller TV" data={mysteryTV} />
+
+    </div>
+  );
+};
+
+export default Tvshow;
